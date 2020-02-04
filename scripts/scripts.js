3bdaa159c00add68fcbe2df545d213199a0d4ff2
Anderson Vieira Lidio <andersonvlidio@gmail.com>
2020-02-04 20:19:48 -0300

    Adicionado eventListeners.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 const containerCards = document.querySelector(".container-cards");
+const loadProductsButton = document.querySelector("#loadProducts")
 
 let url = "frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
 
@@ -6,30 +7,27 @@ async function loadProducts() {
     const response = await fetch('https://' + url)
     const responseJson = await response.json()
     renderProducts(responseJson.products)
-    console.log(responseJson.nextPage)
     url = responseJson.nextPage
 }
 
 function renderProducts(products) {
     products.map(product => {
-        const valueReplaced = product.installments.value.toString().replace("." , ",")
+        const {image, name, description, oldPrice, price, installments} = product
+        const valueReplaced = installments.value.toString().replace("." , ",")
         containerCards.innerHTML += `<div class="card">
                                         <div class="container-img">
-                                            <img src="${product.image}" alt="">
+                                            <img src="${image}" alt="Imagem do produto ${name}" title="${name}" >
                                         </div>
-                                        <h4>${product.name}</h4>
-                                        <p>${product.description}</p>
-                                        <span>De: R$${product.oldPrice},00</span>
-                                        <span>Por: R$${product.price},00</span>
-                                        <span>ou ${product.installments.count}x de R$${valueReplaced}</span>
+                                        <h4>${name}</h4>
+                                        <p>${description}</p>
+                                        <span>De: R$${oldPrice},00</span>
+                                        <span>Por: R$${price},00</span>
+                                        <span>ou ${installments.count}x de R$${valueReplaced}</span>
                                         <button class="button">comprar</button>
                                     </div>`
     })
 
 }
 
-loadProducts()
-
-
-
-
+window.onload = loadProducts
+loadProductsButton.addEventListener("click", loadProducts)
